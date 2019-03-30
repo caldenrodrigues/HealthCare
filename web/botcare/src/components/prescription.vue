235aaf4e5ffa0837fa4e6b00896a443aad045043
rajasi11 <rajasijaiswal11@gmail.com>
2019-03-30 18:40:20 +0530

    final prescription

--- web/botcare/src/components/prescription.vue
+++ web/botcare/src/components/prescription.vue
@@ -15,44 +15,92 @@
                 </v-toolbar>
                 <v-card-text>
                     <v-text-field
-                    prepend-icon="NAME"
-                    label="patient name"
-                    type="text"
-                    v-model="id"
-                    :rules="idRules"
-                    required>
-                    </v-text-field>
-                    <v-text-field
-                    prepend-icon="AGE"
-                    label="patient age"
-                    type="text"
-                    v-model="password"
-                    :rules="passwordRules"
-                    required>
-                    </v-text-field>
-                    <v-text-field
-                    prepend-icon="DATE"
-                    label="DATE OF PRESCRIPTION"
-                    type="text"
-                    v-model="password"
-                    :rules="passwordRules"
-                    required>
-                    </v-text-field>
+
+                                           <v-flex xs12 sm6 d-flex>
+                                    <v-select
+                                      :items="patients"
+                                      label="PATIENT"
+                                    ></v-select>
+                                  </v-flex>
+
+
                     <v-flex xs6>
         <v-subheader>DIAGONISED WITH:</v-subheader>
       </v-flex>
-      <v-flex xs6>
-        <v-select
-          v-model="e1"
-          :items="states"
-          menu-props="auto"
-          label="Select"
-          hide-details
-          
-          single-line
-        ></v-select>
-      </v-flex>
+      <v-flex xs12 sm6  d-flex>
+<v-select
+ :items="dieases"
+ label="SELECT"
+></v-select>
+</v-flex>
+
+
                 </v-card-text>
+ <v-container fluid grid-list-xl>
+   <v-layout wrap align-center>
+                <v-flex xs12 sm6 d-flex>
+         <v-select
+           :items="drugs"
+           label="DRUG"
+         ></v-select>
+       </v-flex>
+       <v-flex xs12 sm6 md3>
+            <v-text-field
+              label="UNIT"
+            ></v-text-field>
+          </v-flex>
+
+
+                       <v-flex xs12 sm6 md3 d-flex>
+                <v-select
+                  :items="doses"
+                  label="DOSE"
+                ></v-select>
+              </v-flex>
+
+ </v-layout>
+    </v-container>
+    <v-flex xs12 sm6>
+     <v-menu
+       ref="menu"
+       v-model="menu"
+       :close-on-content-click="false"
+       :nudge-right="40"
+       :return-value.sync="date"
+       lazy
+       transition="scale-transition"
+       offset-y
+       full-width
+       min-width="290px"
+     >
+       <template v-slot:activator="{ on }">
+         <v-text-field
+           v-model="date"
+           label="NEXT APPOINTMENT DATE"
+           prepend-icon="event"
+           readonly
+           v-on="on"
+         ></v-text-field>
+       </template>
+       <v-date-picker v-model="date" no-title scrollable>
+         <v-spacer></v-spacer>
+         <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+         <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+       </v-date-picker>
+     </v-menu>
+   </v-flex>
+   <v-flex xs12>
+                 <v-textarea
+
+
+                 >
+                   <template v-slot:label>
+                     <div>
+                       INSTRUCTIONS/PRECAUTIONS <small>(optional)</small>
+                     </div>
+                   </template>
+                 </v-textarea>
+               </v-flex>
                 <v-card-actions>
                   <v-spacer></v-spacer>
                   <v-btn
@@ -76,13 +124,15 @@ name: 'Prescription',
 
 data () {
 return {
- e1: 'SELECT',
- items: [
-          { text: 'State 1' },
-        ],
-        states: [
-          'FACTURE', 'CATARACT'
-        ]
+  patients: ['pratik','calden','shadrak','rajasi'],
+dieases: ['CATARACT','FACTURE'],
+        drugs: ['crocin', 'cyclopan', 'combiflam', 'ocaset'],
+        doses: ['once','twice','thrice'],
+        date: new Date().toISOString().substr(0, 10),
+     menu: false,
+     modal: false,
+     menu2: false,
+
 }
 }
 }
