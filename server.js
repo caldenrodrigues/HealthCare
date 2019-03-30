bdd7866ee8766e17a81f39560bed3437bb51d5e3
shadrak98 <gurupnor.shadrak@gmail.com>
2019-03-30 18:11:45 +0530

    changes made

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var server = require('http').Server(app);
-<<<<<<< HEAD
-var io = require('socket.io')(server);
-var mongoose = require('mongoose');
-=======
->>>>>>> 6528d227a7e15b5e64155a633fc75560fe994f56
 app.use(cors());
 const axios = require('axios');
 var mysql = require('mysql');
@@ -93,7 +88,6 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-<<<<<<< HEAD
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/index.html');
 // });
@@ -101,7 +95,6 @@ app.use(bodyParser.urlencoded({
 app.get("/", (req,res)=>{
    return res.send("Welcome to BotCare");
  });
-=======
 app.get("/testQuery", (req,result)=>{
   myvar = "What is the name of the doctor"
   axios.post('http://localhost:5000/testQuery', {
@@ -154,7 +147,6 @@ app.get('/getQuery', (req, result) => {
     console.log(err)
   })
 });
->>>>>>> 6528d227a7e15b5e64155a633fc75560fe994f56
 
 io.on('connection', function (socket) {
     console.log("Socket is connected...");
