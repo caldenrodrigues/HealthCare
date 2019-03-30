0ee6ab1271727d951272c352c65dc0117487c2cc
shadrak98 <gurupnor.shadrak@gmail.com>
2019-03-30 12:39:15 +0530

    started with socket

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -10,10 +12,18 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get("/", (req,res)=>{
+    res.sendFile(__dirname + '/index.html');
 return res.send("Welcome to BotCare");
 });
 
+io.on('connection', function (socket) {
+    socket.emit('start', {hello: 'user'});
+    socket.on('demo', (data)=>{
+        console.log(data);
+    });
+});
+
 //port activation
-app.listen(8081, (req, res) => {
+server.listen(8081, (req, res) => {
     console.log("Listening on 8081");
 });
