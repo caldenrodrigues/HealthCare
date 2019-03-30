f1c151908323fbd21d16be8a219e7c7f6b2a884a
Calden Rodrigues <caldenrodrigues1202@gmail.com>
2019-03-30 12:44:50 +0530

    added accuracy check and best fit for chatterbot

--- Python/chatbot.py
+++ Python/chatbot.py
@@ -2,7 +2,14 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 def get_response():
-	bot = ChatBot('Bot')
+	bot = ChatBot('Bot',
+	logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ])
 	trainer = ListTrainer(bot)
 	while True:
 		usrText = input('You:')
